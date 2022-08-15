016ecf068d8c96a3402375a4e474d37947499d43
Jiovo <justup95@gmail.com>
2022-08-15 20:16:42 +0300

    finish-4

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -60,39 +60,6 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.slider-scrollbar',
   },
-
-
-  //   // Responsive breakpoints
-  //   breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 20
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 30
-  //   },
-  // }
 });
 
-// var mapTitle = document.createElement('div'); mapTitle.className = 'mapTitle';
-// mapTitle.textContent = 'Для активации карты нажмите по ней';
-// wrapMap.appendChild(mapTitle);
-// wrapMap.onclick = function () {
-//   this.children[0].removeAttribute('style');
-//   mapTitle.parentElement.removeChild(mapTitle);
-// }
-// wrapMap.onmousemove = function (event) {
-//   mapTitle.style.display = 'block';
-//   if (event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px';
-//   if (event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px';
-// }
-// wrapMap.onmouseleave = function () {
-//   mapTitle.style.display = 'none';
-// }
 
-// document.querySelector(".pointer-events-none").addEventListener("click", function() {
-//   document.documentElement.classList.toggle(".pointer-events-auto");
-// });
